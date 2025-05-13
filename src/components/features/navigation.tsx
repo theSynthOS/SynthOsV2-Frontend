c1d60f0a88417392f259209039a9c050d3c44075
cheongyeechian <cheongyeechian@gmail.com>
2025-05-13 12:18:56 +0800

    navbar select ux

--- src/components/features/navigation.tsx
+++ src/components/features/navigation.tsx
@@ -2,22 +2,46 @@
 
 import { Home, Award, Wallet } from "lucide-react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
+  const pathname = usePathname();
+  
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+  
   return (
-    <div className="border-t border-gray-800 bg-[#0f0b22]">
-      <div className="flex justify-around py-4">
-        <Link href="/" className="flex flex-col items-center">
-          <Home className="h-6 w-6" />
-          <span className="text-sm mt-1">Home</span>
+    <div className="border-t border-gray-800 bg-[#0f0b22] shadow-lg">
+      <div className="flex justify-around py-4 max-w-md mx-auto">
+        <Link 
+          href="/" 
+          className={`flex flex-col items-center ${isActive('/home') ? 'text-white' : 'text-gray-500'}`}
+        >
+          <div className={`${isActive('/home') ? 'bg-purple-600/20  rounded-full' : ''}`}>
+            <Home className={`h-6 w-6 ${isActive('/home') ? 'text-purple-500' : ''}`} />
+          </div>
+          <span className="text-xs mt-1">Home</span>
         </Link>
-        <Link href="/rewards" className="flex flex-col items-center text-gray-500">
-          <Award className="h-6 w-6" />
-          <span className="text-sm mt-1">Feeds</span>
+        
+        <Link 
+          href="/rewards" 
+          className={`flex flex-col items-center ${isActive('/rewards') ? 'text-white' : 'text-gray-500'}`}
+        >
+          <div className={`${isActive('/rewards') ? 'bg-purple-600/20  rounded-full' : ''}`}>
+            <Award className={`h-6 w-6 ${isActive('/rewards') ? 'text-purple-500' : ''}`} />
+          </div>
+          <span className="text-xs mt-1">Feeds</span>
         </Link>
-        <Link href="/holding" className="flex flex-col items-center text-gray-500">
-          <Wallet className="h-6 w-6" />
-          <span className="text-sm mt-1">Holdings</span>
+        
+        <Link 
+          href="/holding" 
+          className={`flex flex-col items-center ${isActive('/holding') ? 'text-white' : 'text-gray-500'}`}
+        >
+          <div className={`${isActive('/holding') ? 'bg-purple-600/20  rounded-full' : ''}`}>
+            <Wallet className={`h-6 w-6 ${isActive('/holding') ? 'text-purple-500' : ''}`} />
+          </div>
+          <span className="text-xs mt-1">Holdings</span>
         </Link>
       </div>
     </div>
