d3bcb7942c00a91421ef0798ef363575f958cd8d
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2025-05-29 18:13:03 +0800

    feat: add posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
 import DynamicProviders from "@/components/providers/dynamic-providers";
+import { PostHogProvider } from "@/components/providers/posthog-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export const metadata: Metadata = {
 
 // export const viewport = {
 //   themeColor: "#0f0b22",
-// };
+//};
 
 export default function RootLayout({
   children,
@@ -48,9 +49,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <DynamicProviders>
+        <PostHogProvider>
+          <DynamicProviders>
             {children}
-        </DynamicProviders>
+          </DynamicProviders>
+        </PostHogProvider>
       </body>
     </html>
   );
