27beac02272c18cce2a5a1315b132ffae75d1447
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2025-05-29 21:10:14 +0800

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,19 @@ import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
 import DynamicProviders from "@/components/providers/dynamic-providers";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-  display: 'swap',
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-  display: 'swap',
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -33,15 +35,21 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
         <title>SynthOS V2</title>
-        <meta name="description" content="SynthOS V2 - Your gateway to the future of DeFi" />
+        <meta
+          name="description"
+          content="SynthOS V2 - Your gateway to the future of DeFi"
+        />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         {/* <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" /> */}
       </head>
@@ -49,7 +57,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <DynamicProviders>
-            {children}
+          {children}
+          <SpeedInsights />
+          <Analytics />
         </DynamicProviders>
       </body>
     </html>
