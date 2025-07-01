144bc316629d3604484eddc77e94f0ef1ba3c187
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2025-07-01 22:59:57 +0800

    fix: build errors

--- src/app/api/protocol-pairs-apy/[id]/route.ts
+++ src/app/api/protocol-pairs-apy/[id]/route.ts
@@ -5,10 +5,10 @@ dotenv.config();
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = params;
+    const { id } = await params;
 
     if (!id) {
       return NextResponse.json(
